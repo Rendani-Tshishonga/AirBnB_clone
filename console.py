95046ad2d6878dac212dea1fffa4e186f720629b
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-05-18 11:09:07 -0700

    console

--- console.py
+++ console.py
@@ -7,12 +7,17 @@ command interpreter.
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb)"
-    """ EOF character to exit the program """
-    do_EOF = do_quit()
+    prompt = "(hbnb) "
 
 def do_quit(self, arg):
     return True
+
+def do_EOF(self, arg):
+    return True
+
+def emptyline(self):
+    pass
+
 
 if __name__ == '__main__':
     HBNBCommand(cmd.Cmd).cmdloop()
