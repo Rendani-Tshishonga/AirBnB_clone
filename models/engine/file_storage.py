60e208448b832ac3dd4c445f19431df136bbbfd5
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-05-20 05:55:48 -0700

    Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import json
+import os
 
 class FileStorage:
     __file_path = "file.json"
@@ -22,10 +23,11 @@ def save(self):
 
 def reload(self):
     """ Deserializes the JSON file to __objects."""
-    if FileStorage.__file_path != "":
-        with open('FileStorage.__file_path', 'r') as f:
-            FileStorage.__object = json.load(f)
-        FileStorage.__object = FileStorage(FileStorage.__object)
+    if not os.path.isfile(FileStorage.__filepath):
+        return
+    with open('FileStorage.__file_path', 'r') as f:
+        my_dict = json.load(f)
+        FileStorage.__object = {for k, v in my_dict.items()}
 
 def class_list(self):
     class_list = ["BaseModel", "City", "User", "Place",\
