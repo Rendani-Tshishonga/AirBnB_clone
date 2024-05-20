c4a6d3b71709f8381f8447e27130631a89436743
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-05-20 05:19:04 -0700

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,20 @@ import datetime
 from models import storage
 
 class BaseModel:
+    """
+    This is the class of the Base Model
+    *args will not be used.
+
+    if kwargs is not empty:
+        each key of this dictionary is an attribute name
+        each value of this dictionary is the value of this attribute name
+        Warning: created_at and updated_at are strings in this dictionary,
+        but inside your BaseModel instance is working with datetime object.
+        You have to convert these strings into datetime object.
+        Tip: you know the string format of these datetime
+    """
     def __init__(self, *args, **kwargs):
+
         if kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
@@ -22,14 +35,30 @@ class BaseModel:
             storage.new(self)
 
 def __str__(self):
+    """
+    Should return [<class name>] (<self.id>) <self.__dict__>
+    """
     return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
 
 def save(self):
+    """
+    Updates the public instance variable
+    with the current date. Save the current
+    date of the storage instance 
+    """
     updated_at = datetime.now()
     storage.save(self)
 
 def to_dict(self):
+    """
+     returns a dictionary containing all keys/values of __dict__ of the instance
+        by using self.__dict__, only instance attributes set will be returned.
+        a key __class__ must be added to this dictionary with the class name of the object
+        created_at and updated_at must be converted to string object in ISO format
+        format: %Y-%m-%dT%H:%M:%S.%f
+        you can use isoformat() of datetime object
+    """
     my_dict = self.__dict__.copy()
     my_dict["__class__"] = type((self).__name__)
     my_dict["created_at"] = my_dict["created_at"].isoformat().\
